b3a389e5476a45ec91ac91df4aba2e45671e8e8c
Krimy <mrkrimy@gmail.com>
2025-02-23 19:50:32 +0800

    Add cat functionality

--- animal.py
+++ animal.py
@@ -1,7 +1,15 @@
 import sys
+def cat():
+	print("I am a cat")
+
 def default():
 	print("Hello World")
+
 def main():
-	default()
+	if sys.argv[1] == 'cat':
+		cat()
+	else:
+		default()
+
 if __name__ == '__main__':
     main()
